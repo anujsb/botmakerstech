313cd75191e79623bfbdda948903a959841f14a2
anujsb <141896390+anujsb@users.noreply.github.com>
2024-12-06 20:53:47 +0530

    about section

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -5,16 +5,16 @@ import { FloatingNav } from "../ui/floating-navbar";
 export function Header() {
   const navItems = [
     { name: "Home", link: "/" },
-    { name: "Robo Playzone", link: "/#about" },
-    { name: "InnoTech Academy", link: "/#features" },
-    { name: "Services", link: "https://21bubbles.com/" },
+    { name: "Robo Playzone", link: "/" },
+    { name: "InnoTech Academy", link: "/" },
+    { name: "Services", link: "/" },
     
     {
       name: "Insights",
-      link: "https://cal.com/anuj-bhuyar-gj0xf3/21bubbles-contact?date=2024-10-30&month=2024-10",
+      link: "/",
     },
-    { name: "About Us", link: "https://21bubbles.com/" },
-    { name: "Contact Us", link: "https://21bubbles.com/" },
+    { name: "About Us", link: "/about" },
+    { name: "Contact Us", link: "/about" },
   ];
 
   return (
